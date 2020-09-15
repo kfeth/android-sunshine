c3ccac31c3b9ef6746fa921d16166eecd5b41388
Karl Fetherstonhaugh <>
2020-09-15 14:38:02 +0100

    • Support for Dark Mode. Minor UI tweaks (explicit bg colors removed)

--- app/src/main/java/com/kfeth/sunshine/SunshineApp.kt
+++ app/src/main/java/com/kfeth/sunshine/SunshineApp.kt
@@ -1,7 +1,14 @@
 package com.kfeth.sunshine
 
 import android.app.Application
+import androidx.appcompat.app.AppCompatDelegate
 import dagger.hilt.android.HiltAndroidApp
 
 @HiltAndroidApp
-class SunshineApp : Application()
+class SunshineApp : Application() {
+
+    override fun onCreate() {
+        super.onCreate()
+        AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_FOLLOW_SYSTEM)
+    }
+}
