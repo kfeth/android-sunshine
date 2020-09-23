9933bc1959457d2aa48e564ec7825c18fa106b18
Karl Fetherstonhaugh <>
2020-09-23 17:20:05 +0100

    • Refactor DetailsFragment to match the structure of the other frags

--- app/src/main/java/com/kfeth/sunshine/ui/DetailsFragment.kt
+++ app/src/main/java/com/kfeth/sunshine/ui/DetailsFragment.kt
@@ -8,17 +8,13 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import com.google.android.material.tabs.TabLayoutMediator
-import com.kfeth.sunshine.R
 import com.kfeth.sunshine.adapters.CurrentWeatherAdapter
 import com.kfeth.sunshine.adapters.ForecastAdapter
 import com.kfeth.sunshine.databinding.FragmentDetailsBinding
-import com.kfeth.sunshine.utilities.bind
 import com.kfeth.sunshine.utilities.showSnackBar
 import com.kfeth.sunshine.viewmodels.DetailsViewModel
 import dagger.hilt.android.AndroidEntryPoint
-import kotlinx.android.synthetic.main.fragment_details.forecastRecyclerView
 import kotlinx.android.synthetic.main.fragment_details.root
-import kotlinx.android.synthetic.main.fragment_details.viewPager
 
 @AndroidEntryPoint
 class DetailsFragment : Fragment() {
@@ -30,31 +26,33 @@ class DetailsFragment : Fragment() {
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return bind<FragmentDetailsBinding>(R.layout.fragment_details, container).apply {
+        val binding = FragmentDetailsBinding.inflate(inflater, container, false)
+        val currentWeatherAdapter = CurrentWeatherAdapter()
+        val forecastAdapter = ForecastAdapter()
+
+        binding.apply {
             lifecycleOwner = this@DetailsFragment
             viewModel = this@DetailsFragment.viewModel
-            forecastRecyclerView.adapter = ForecastAdapter()
+            forecastList.adapter = forecastAdapter
+            currentWeatherViewPager.adapter = currentWeatherAdapter
+            TabLayoutMediator(tabLayout, currentWeatherViewPager) { _, _ -> }.attach()
+        }
 
-            viewPager.adapter = CurrentWeatherAdapter()
-            TabLayoutMediator(tabLayout, viewPager) { _, _ -> }.attach()
-        }.root
+        subscribeUi(currentWeatherAdapter, forecastAdapter)
+        return binding.root
     }
 
-    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+    private fun subscribeUi(
+        currentWeatherAdapter: CurrentWeatherAdapter,
+        forecastAdapter: ForecastAdapter
+    ) {
         viewModel.apply {
-            currentWeather.observe(viewLifecycleOwner, {
-                (viewPager.adapter as CurrentWeatherAdapter).replace(it)
-            })
-
-            forecast.observe(viewLifecycleOwner, {
-                (forecastRecyclerView.adapter as ForecastAdapter).submitList(it)
-            })
-
-            errorMessage.observe(viewLifecycleOwner, { root.showSnackBar(it) })
-
-            title.observe(viewLifecycleOwner, {
+            currentWeather.observe(viewLifecycleOwner) { currentWeatherAdapter.replace(it) }
+            forecast.observe(viewLifecycleOwner) { forecastAdapter.submitList(it) }
+            errorMessage.observe(viewLifecycleOwner) { root.showSnackBar(it) }
+            title.observe(viewLifecycleOwner) {
                 (activity as AppCompatActivity).supportActionBar?.title = it
-            })
+            }
         }
     }
 }
